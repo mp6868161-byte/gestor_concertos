f694d853c52e522a87c9c16d5ebcee8c3a6b140b
Pekka (Martim.B) <mp6868161@gmail.com>
2026-04-12 22:11:38 +0100

    Update and rename main.py to Artista.py

---
+++ src/Artista.py
@@ -0,0 +1,37 @@
+def programa_concerto_escola():
+    # Dados imutáveis do evento
+    agenda = {
+        "Bon Jovi": {"hora": "18:00", "info": "Hard Rock clássico /."},
+        "AC/DC": {"hora": "20:00", "info": "Rock n' Roll puro /."},
+        "Aerosmith": {"hora": "22:00", "info": "Blues-Rock /."},
+        "Linkin Park": {"hora": "00:00", "info": "Rock Alternativo /."}
+    }
+
+    while True:
+        print("\n" + "="*35)
+        print("      ROCK NA ESCOLA")
+        print("="*35)
+        print("1. Informações dos Artistas")
+        print("2. Horário das Atuações")
+        print("3. Sair")
+
+        opcao = input("\nEscolha uma opção: ")
+
+        if opcao == "1":
+            print("\n--- SOBRE OS ARTISTAS ---")
+            for banda, dados in agenda.items():
+                print(f"{banda}: {dados['info']}")
+
+        elif opcao == "2":
+            print("\n--- HORÁRIOS ---")
+            for banda, dados in agenda.items():
+                print(f"{dados['hora']} -> {banda}")
+
+        elif opcao == "3":
+            print("Saindo...")
+            break
+        else:
+            print("Opção inválida.")
+
+if __name__ == "__main__":
+    programa_concerto_escola()
